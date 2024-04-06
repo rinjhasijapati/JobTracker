2eb3d7de83a923f8726c6ad0fe3c03bbeeacd427
swornimthapa <swornimtheking@gmail.com>
2024-04-06 17:34:01 +0545

    authentication done

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
-use App\Models\User;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,65 +15,17 @@ use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 */
 
 Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware('auth')->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/register', function () {
-    return view('register');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
 
-Route::get('/confirm/logout', function () {
-    if(Auth::check()){
-        return "<script>if(confirm('Are you sure you want to exit?')) { window.location.href = '/logout'; } else { window.location.href = '/';}</script>";
-    }else{
-        return view('login');
-    }
-})->name('confirm-logout');
-
-
-Route::get('/logout', function () {
-    // if(Auth::check()){
-    //     return redirect(route('confirm-logout'));
-    // }else{
-    //     return view('login');
-    // }
-    Auth::logout();
-    return view('login');
-})->name('logout');
-
-Route::post('/register-user', function (Request $req) {
-
-    $req->validate([
-        'email' => 'required',
-        'name' => 'required',
-        'password' => 'required',
-    ]);
-    $parsedData = [
-        'email' => $req->email,
-        'password' => bcrypt($req->password),
-        'name' => $req->name,
-    ];
-    User::create($parsedData);
-    return redirect(route('login'));
-})->name('register-user');
-
-
-Route::post('/signin', function (Request $req) {
-    $req->validate([
-        'email' => ['required', 'email'],
-        'password' => 'required',
-    ]);
-
-    if (Auth::attempt(['email' => $req->email, 'password' => $req->password])) {
-        // password is correct
-        return redirect(route('dashboard'));
-    } else {
-       
-        return response(route('login'));
-
-    }
-
-});
+require __DIR__.'/auth.php';
