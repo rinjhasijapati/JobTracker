cd48d12205f2dcb8bedc34f06194ccaf19f8b4ee
rinjhasijapati <aastha_sijapati@yahoo.com>
2024-04-19 22:54:28 +0545

    leads details completed

--- resources/views/showleaddetails.blade.php
+++ resources/views/showleaddetails.blade.php
@@ -4,71 +4,87 @@
             details
         </h2>
     </x-slot>
-    <div class="max-w-3xl mx-auto">
-        <div>
+    <div class="flex flex-wrap items-start">
+        <div class="max-w-4xl mx-auto pl-20 ml-16">
+            <div>
+                <br>
+                <h2 class="text-3xl  dark:text-white pb-2">American Heritage Girls</h2>
+                <h1 class="text-5xl font-extrabold dark:text-white pb-3">Application Developer</h1>
+                <div class="flex flex-wrap items-center">
+                    <p class="text-gray-500 dark:text-gray-400 pr-5">
+                        <a href="#" class="mr-4">
+                            <i class="fa fa-globe" aria-hidden="true"></i> American Heritage Girls Website
+                        </a>
+                    </p>
+
+                    <p class="text-gray-500 dark:text-gray-400 pr-5">
+                        <a href="#" class="mr-4">
+                            <i class="fa fa-map-marker" aria-hidden="true"></i> Remote
+                        </a>
+                    </p>
+
+                    <p class="text-gray-500 dark:text-gray-400 pr-5">
+                        <a href="#" class="mr-4">
+                            <i class="fa fa-usd" aria-hidden="true"></i> $22-$24 an hour
+                        </a>
+                    </p>
+
+                    <p class="text-gray-500 dark:text-gray-400 pr-5">
+                        <a href="#" class="mr-4">
+                            <i class="fa fa-briefcase" aria-hidden="true"></i> Full-Time
+                        </a>
+                    </p>
+
+
+                </div>
+            </div>
             <br>
-            <h2 class="text-3xl  dark:text-white pb-2">American Heritage Girls</h2>
-            <h1 class="text-5xl font-extrabold dark:text-white pb-3">Application Developer</h1>
-            <div class="flex flex-wrap items-center">
-                <p class="text-gray-500 dark:text-gray-400 pr-5">
-                    <a href="#" class="mr-4">
-                        <i class="fa fa-globe" aria-hidden="true"></i> American Heritage Girls Website
-                    </a>
+            <div>
+                <h2 class="text-2xl dark:text-white pb-2">Company Summary</h2>
+                <p class="text-gray-500 dark:text-gray-400">Add a quick summary about this company.</p>
+            </div>
+            <br>
+            <div>
+                <h2 class="text-2xl dark:text-white pb-2">Job Description</h2>
+            </div>
+
+            <div class="bg-white shadow-md rounded-md p-6">
+                <p class="mb-3 text-gray-700 dark:text-gray-400">Position title: Application Developer</p><br>
+                <p class="mb-3 text-gray-700 dark:text-gray-400">Department: IT Status: Full Time, Non-Exempt</p><br>
+                <p class="mb-3 text-gray-700 dark:text-gray-400">Location: Remote</p><br>
+                <p class="mb-3 text-gray-700 dark:text-gray-400">Reports to: Sr. Director of IT & Volunteer Services Works with: AHG Staff and Volunteers Pay</p><br>
+                <p class="mb-3 text-gray-700 dark:text-gray-400">Range: $22 - $24 per hour</p><br>
+                <p class="mb-3 text-gray-700 dark:text-gray-400">
+                    Position Description:<br>
+
+                    Exhibits a Christ-like servant leadership spirit while developing and maintaining all databases and websites in the environment. This role is the primary administrator for all development applications and projects.
+
+                    Key Responsibilities and Objectives Domain monitoring, and Hosting
+
+                    A. Manage GoDaddy and Hover accounts; purchase domains; renew domains B. Ensure site security; SSL Updates
+
+                    Development, Architecture, Programming
+
+                    C. Typical programming: HTML/PHP/CSS/JQUERY/MySQL D. Manage postgresql database E. Bootstrap Framework F. WordPress and other CMS updates G. General redesign and/or maintenance of current websites
+
+                    Required Skills, Education, and Experience A. Passion for serving God in the AHG ministry. B. Agreement with AHG’s Statement of Faith; must exhibit and maintain a strong relationship with Christ and a positive faith attitude. C. Associates degree in information technology Bachelor’s degree or 0 – 2 years’ experience in information technology, computer science or related field. D. Strong analytical skills and methodical troubleshooting. E. Ability to communicate complex technical issues to non-technical individuals. F. Exceptional oral and written communication skills within a diverse office environment. G. Capacity to prioritize a variety of tasks and a heavy volume of work. H. Dexterity with learning new skills and staying current with rapidly changing technology. I. Ability to work independently and as a member of a team. J. Excellent analytical and problem-solving skills.
                 </p>
-
-                <p class="text-gray-500 dark:text-gray-400 pr-5">
-                    <a href="#" class="mr-4">
-                        <i class="fa fa-map-marker" aria-hidden="true"></i> Remote
-                    </a>
-                </p>
-
-                <p class="text-gray-500 dark:text-gray-400 pr-5">
-                    <a href="#" class="mr-4">
-                        <i class="fa fa-usd" aria-hidden="true"></i> $22-$24 an hour
-                    </a>
-                </p>
-
-                <p class="text-gray-500 dark:text-gray-400 pr-5">
-                    <a href="#" class="mr-4">
-                        <i class="fa fa-briefcase" aria-hidden="true"></i> Full-Time
-                    </a>
-                </p>
-
-
             </div>
         </div>
-        <br><br>
-        <div>
-            <h2 class="text-2xl dark:text-white pb-2">Company Summary</h2>
-            <p class="text-gray-500 dark:text-gray-400">Add a quick summary about this company.</p>
-        </div>
-        <br><br>
-        <div>
-            <h2 class="text-2xl dark:text-white pb-2">Job Description</h2>
-        </div>
+
+        <div class="bg-white shadow-md rounded-md p-6 max-w-sm mt-80 mr-36">
+            <div class="pb-10">
+            <h2 class="text-3xl  dark:text-white pb-2">Contacts</h2>
+            <p class="text-gray-500 dark:text-gray-400 pb-2">You haven't added any contacts from this company</p>
+            <button type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-3">Add a contact</button>
+            </div>
+            <h2 class="text-3xl  dark:text-white pb-2">Documents</h2>
+            <p class="text-gray-500 dark:text-gray-400 pb-2">No documents assigned to this lead</p>
+            <button type="button" class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 flex items-center focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+    <i class="fa fa-file mr-2" aria-hidden="true"></i> Add a document
+</button>
+
         
-        <div class="bg-white shadow-md rounded-md p-6">
-            <p class="mb-3 text-gray-700 dark:text-gray-400">Position title: Application Developer</p><br>
-            <p class="mb-3 text-gray-700 dark:text-gray-400">Department: IT Status: Full Time, Non-Exempt</p><br>
-            <p class="mb-3 text-gray-700 dark:text-gray-400">Location: Remote</p><br>
-            <p class="mb-3 text-gray-700 dark:text-gray-400">Reports to: Sr. Director of IT & Volunteer Services Works with: AHG Staff and Volunteers Pay</p><br>
-            <p class="mb-3 text-gray-700 dark:text-gray-400">Range: $22 - $24 per hour</p><br>
-            <p class="mb-3 text-gray-700 dark:text-gray-400">
-                Position Description:<br>
-
-                Exhibits a Christ-like servant leadership spirit while developing and maintaining all databases and websites in the environment. This role is the primary administrator for all development applications and projects.
-
-                Key Responsibilities and Objectives Domain monitoring, and Hosting
-
-                A. Manage GoDaddy and Hover accounts; purchase domains; renew domains B. Ensure site security; SSL Updates
-
-                Development, Architecture, Programming
-
-                C. Typical programming: HTML/PHP/CSS/JQUERY/MySQL D. Manage postgresql database E. Bootstrap Framework F. WordPress and other CMS updates G. General redesign and/or maintenance of current websites
-
-                Required Skills, Education, and Experience A. Passion for serving God in the AHG ministry. B. Agreement with AHG’s Statement of Faith; must exhibit and maintain a strong relationship with Christ and a positive faith attitude. C. Associates degree in information technology Bachelor’s degree or 0 – 2 years’ experience in information technology, computer science or related field. D. Strong analytical skills and methodical troubleshooting. E. Ability to communicate complex technical issues to non-technical individuals. F. Exceptional oral and written communication skills within a diverse office environment. G. Capacity to prioritize a variety of tasks and a heavy volume of work. H. Dexterity with learning new skills and staying current with rapidly changing technology. I. Ability to work independently and as a member of a team. J. Excellent analytical and problem-solving skills.
-            </p>
         </div>
     </div>
-
 </x-app-layout>
